d3c5c3f243a808ee17159498c3d1a0bc70216de9
miguelmc0217 <39894298+miguelmc0217@users.noreply.github.com>
2018-11-07 11:47:42 -0400

    primer

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Miguel Mercado - 2016-3990");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
